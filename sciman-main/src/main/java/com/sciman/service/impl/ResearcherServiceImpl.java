acce04cf8bcc13b080777f2cfb63bc0d452f64cb
Gorun <307532500@qq.com>
2023-12-24 16:49:38 +0800

    添加查询researcher名字的日志

--- sciman-main/src/main/java/com/sciman/service/impl/ResearcherServiceImpl.java
+++ sciman-main/src/main/java/com/sciman/service/impl/ResearcherServiceImpl.java
@@ -5,17 +5,20 @@ import com.sciman.dto.researcher.QueryParam;
 import com.sciman.service.ResearcherService;
 import com.sciman.vo.person.ResearcherView;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ResearcherServiceImpl implements ResearcherService {
     private final ResearcherMapper researcherMapper;
 
     @Override
     public List<ResearcherView> listResearcherViewFor(QueryParam queryParam) {
+        log.info(queryParam.toString());
         queryParam.normalize();
         return researcherMapper.listResearcherView(queryParam.getNameFilter());
     }
