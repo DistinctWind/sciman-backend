0391f2dad8945d792ff61886463704f29c685d02
Gorun <307532500@qq.com>
2023-12-26 20:47:03 +0800

    添加秘书查询参数

---
+++ sciman-main/src/main/java/com/sciman/dto/secretary/QueryParam.java
@@ -0,0 +1,17 @@
+package com.sciman.dto.secretary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QueryParam {
+    private String nameFilter;
+    private Integer page;
+    private Integer pageSize;
+    public void normalize() {
+        if (nameFilter != null && nameFilter.isEmpty()) nameFilter = null;
+    }
+}
