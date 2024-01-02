5d73531e256f3a18c25ce8115a5ee5316e537b95
Gorun <307532500@qq.com>
2024-01-02 22:32:25 +0800

    添加查询所有科研成果信息的方法

--- sciman-main/src/main/java/com/sciman/service/AchievementService.java
+++ sciman-main/src/main/java/com/sciman/service/AchievementService.java
@@ -2,7 +2,11 @@ package com.sciman.service;
 
 import com.sciman.dto.achievement.AchievementQueryParam;
 import com.sciman.dto.achievement.AchievementQueryResult;
+import com.sciman.vo.achievement.AchievementView;
+
+import java.util.List;
 
 public interface AchievementService {
     AchievementQueryResult getAchievementList(AchievementQueryParam queryParam);
+    List<AchievementView> getAllAchievement();
 }
